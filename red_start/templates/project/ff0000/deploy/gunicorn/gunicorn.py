f22d100e97d6326871ad1f5d7d387e305f1250ee
Peter <peter.volpe@ff0000.com>
2012-03-29 17:45:34 -0700

    Fix for Gunicorn 0.14
    Gunicorn 0.14 changed the way that django settings are loaded breaking our setup. This should work for both 0.13 and 0.14

--- red_start/templates/project/ff0000/deploy/gunicorn/gunicorn.py
+++ red_start/templates/project/ff0000/deploy/gunicorn/gunicorn.py
@@ -1,3 +1,5 @@
+import os
+pythonpath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../project/'))
 bind = "127.0.0.1:8000"
 
 # Make sure to tune
@@ -5,3 +7,4 @@ workers = 10
 
 loglevel = "WARNING"
 logfile = "/var/log/gunicorn/errors.log"
+django_settings = "settings"
